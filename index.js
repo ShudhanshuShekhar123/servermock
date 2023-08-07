ee28e16680bb4345a9590dffff78b0e991da9976
shudhanshuShekhar <shudhanshuraz01@gmail.com>
2023-08-07 14:58:17 +0530

    add

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const jsonServer = require('json-server')
 const cors = require('cors')
 const path = require('path')
 
+
 const server = jsonServer.create()
+
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
+
 const middlewares = jsonServer.defaults()
-server.use(cors({
-  origin: '*'
-}));
-server.use(jsonServer.bodyParser)
+
+// server.use(jsonServer.bodyParser)
+server.use(cors());
 server.use(middlewares)
 server.use(router)
 
